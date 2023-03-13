45890e72d7e851796637ce34cc132817c4809737
Prince Lee Nna <prince_lee.nna85@ymail.com>
2023-03-13 07:42:47 +0100

    Update Tables.jsx

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -6,7 +6,7 @@ import Converter from 'timestamp-conv'
 import convert from 'ethereum-unit-converter'
 
 
-const transactionApi = 'https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0x5a79be6cdce26bc853d72919bf98a0378641b607&address=0x000000000000000000000000000000000000dEaD&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=RM1PBIHRH9F5JVJYH7ENZI6SWZMGXU3FTY';
+const transactionApi = 'https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0x5a79be6cdce26bc853d72919bf98a0378641b607&address=0x000000000000000000000000000000000000dEaD&page=1&offset=100&startblock=0&endblock=27000000&sort=desc&apikey=2KBH34YNQ4QHV4E5RHF5Y4QBCDWG5RJ5IP';
 
 
 const Tables = ({tableClass, tableTitle, pagination}) => {
@@ -14,7 +14,7 @@ const Tables = ({tableClass, tableTitle, pagination}) => {
 
     const columns = [
         {
-            name: 'Date',
+            name: 'Time',
             selector: row => row.timeStamp,
 
         },
@@ -61,8 +61,8 @@ const Tables = ({tableClass, tableTitle, pagination}) => {
                     return returningData;
 
                 });
-                // sort data in descending order
-                setTableData(newApidata.reverse());
+                
+                setTableData(newApidata);
             })
             .catch((err) => {
                 console.log(err.message);
